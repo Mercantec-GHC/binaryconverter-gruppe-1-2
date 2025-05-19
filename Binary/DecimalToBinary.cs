8950c3df93f631d550ac32d163b61be01c3b8b7d
Matgjo <mathiash@gjortz.net>
2025-05-19 08:27:43 +0200

    Update DecimalToBinary.cs

--- Binary/DecimalToBinary.cs
+++ Binary/DecimalToBinary.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BinaryConverter
 {
@@ -11,13 +13,49 @@ namespace BinaryConverter
         public static void Run()
         {
             Console.WriteLine("Decimal to Binary Converter");
-            ConvertDecimalToBinary("127.0.0.1");
+            string randomnumber = Console.ReadLine();
+
+            Console.WriteLine("Enter decimal here: ");
+            string[] oktetter = randomnumber.Split('.'); // Opdeler oktetter med et ".".
+            Console.Write(ConvertDecimalToBinary(int.Parse(oktetter[0]))); //Outputs "oktteter" and converts string to integer
+            Console.Write("."); // Splits oktteter with a "."
+            Console.Write(ConvertDecimalToBinary(int.Parse(oktetter[1])));
+            Console.Write(".");
+            Console.Write(ConvertDecimalToBinary(int.Parse(oktetter[2])));
+            Console.Write(".");
+            Console.Write(ConvertDecimalToBinary(int.Parse(oktetter[3])));
+
         }
 
-        public static string ConvertDecimalToBinary(string decimalIP)
+        public static string ConvertDecimalToBinary(int decimalIP)
         {
-            Console.WriteLine($"Example: {decimalIP} => 01111111.00000000.00000000.00000001");
-            return "01111111.00000000.00000000.00000001";
+            string binarycontainer = ""; //An empty string to contain a binary number 
+
+
+            int[] decimaltal = { 128, 64, 32, 16, 8, 4, 2, 1 }; //Array of decimal numbers to be converted to binary
+
+
+            for (int i = 0; i < decimaltal.Length; i++) // the loop starts with i  set to 0 and runs until i is less than the length of the array,
+                                                        // After each loop iteration, i is increased by 1
+            {
+
+            if (decimaltal[i]<= decimalIP) //cheks if the decimal number is less than or equal to the decimalIP
+                {
+                decimalIP -= decimaltal[i]; //If the condition is true, the decimalIP is decrease by the value of the current element in the array
+                    binarycontainer = binarycontainer + "1"; //If conditions are met binarycontainer + 1
+            }
+            else
+            {
+                binarycontainer = binarycontainer + "0"; // If conditions are not met, it will post 0
+            }
+
+                 
+            }
+            return binarycontainer;
+
+
+
+
         }
     }
 }
